bbccce9f9fb60afc37b1d124de23eaefaa652623
Kevin Boucherit <boucherit.kevin@gmail.com>
2019-03-20 17:30:46 +0100

    Stephane' s list

---
+++ wilders.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+
+
+console.log(chalk.yellow("stephane"))
+console.log(chalk.red("stephane"))
+console.log(chalk.white("stephane"))
+console.log(chalk.green("stephane"))
